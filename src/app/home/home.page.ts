665e8dba3e8e29b2db67d165a14b11d8a4cd5dab
Renanzen <renan.soares33@etec.sp.gov.br>
2024-08-23 14:39:42 -0300

    Renan - Colocando API com PHP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,36 +10,36 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  recado = {
-    assunto : null
-  }
-  constructor(private crudservice: CrudService){ 
+  
+  isLoading: boolean = false;
+  funcionarios: any;
+
+  getFuncionarios(){
+    this.isLoading = true;
+	
+		let funcionario = { CodFun: '6' };
+		let funcionarios: any;
     
-}
-  enviar(){
-    this.crudservice.insert(this.recado, (recado))
+    fetch('http://localhost/api/v1/listar_funcionario.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(funcionario)
+			}
+		)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      this.funcionarios = response.funcionarios
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.isLoading = false;
+    })
   }
-  pessoa = {
-    nome: 'Pombinha da paz Silva',
-    foto: 'https://professionalmoron.com/wp-content/uploads/2012/05/alpaca-985158_640.jpg',
-    objetivo: 'programar HTML & CSS',
-    contato:{
-    email: 'pombinho@hotmail.com',
-    telefone: '(11)99992992',
-    github: 'github.com/pombinha',
-    linkedin: 'linkedin.com/pombinha'
-    },
-    softskills:[
-      'Comunicação',
-      'Proatividade',
-      'Trabalho em Grupo'
-    ],
-    formacao:[
-      {ano_iniciado: '2021', ano_fim: '2024', instituicao: 'Etec Sales Gomes', curso: 'Técnico Desenvolvimento de sistemas'},
-      {ano_iniciado: '2025', ano_fim: '2027', instituicao: 'Faculdade de tecnologia - Fatec', curso: 'Superior em Desenvolvimento de sistemas'}
-    ],
-    projeto: [
-      {ano: '2023', instituicao: 'Etec Sales Gomes', nome_projeto: 'Etec de Portas Abertas', descricao: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, impedit. Quam provident neque, mollitia iure vero doloribus quaerat, maxime perferendis sequi possimus corrupti illum error placeat explicabo id saepe ex.'}
-    ]
-  }
+
 }
